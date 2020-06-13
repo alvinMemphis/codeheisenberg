bfbf10543ca8b112861c5927fd621868ec8a2843
alvin <alvinnsongambi9@gmail.com>
2020-06-13 09:07:15 +0300

    privacy policy init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home/Home';
+import PrivacyPolicy from './Screens/privacypolicy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+        <Home/>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/privacy">
+          <PrivacyPolicy />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
